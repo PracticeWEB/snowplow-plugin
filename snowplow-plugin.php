ea1bb4182a915aea913add2b383dfbd4603ead04
David Robinson <David.Robinson@practiceweb.co.uk>
2017-06-02 11:23:57 +0100

    COOKIE_DOMAIN isn't set by default so derive domain when it's not available

--- snowplow-plugin.php
+++ snowplow-plugin.php
@@ -17,10 +17,11 @@ function wp_snowplow_add_scripts() {
     if ($collector && $application_id) {
 		wp_enqueue_script( 'snowplow_loader', plugin_dir_url( __FILE__ ) . 'js/snowplowLoader.js' );
 		// Use localization to parameterise the script.
+	    $cookie_domain = wp_snowplow_get_cookie_domain();
 		$parameters = array(
 			'collector' => $collector,
 			'application_id' => $application_id,
-			'cookie_domain' => COOKIE_DOMAIN,
+			'cookie_domain' => $cookie_domain,
 		);
 		wp_enqueue_script( 'snowplow_tracker', plugin_dir_url( __FILE__ ) . 'js/snowplowTracker.js' );
 	    wp_localize_script('snowplow_tracker', 'sp_data', $parameters);
@@ -33,26 +34,37 @@ function wp_snowplow_add_scripts() {
  * App ID
  *
  */
-function snowplow_settings_init() {
-	add_settings_section( 'snowplow', 'Snowplow Settings', 'snowplow_settings_section', 'general' );
+function wp_snowplow_settings_init() {
+	add_settings_section( 'snowplow', 'Snowplow Settings', 'wp_snowplow_settings_section', 'general' );
 
-	add_settings_field( 'sp_collector', 'Snowplow Collector', 'snowplow_setting_collector', 'general', 'snowplow');
+	add_settings_field( 'sp_collector', 'Snowplow Collector', 'wp_snowplow_setting_collector', 'general', 'snowplow');
 	register_setting('general', 'sp_collector');
-	add_settings_field( 'sp_appid', 'Snowplow Application Id', 'snowplow_setting_appid', 'general', 'snowplow');
+	add_settings_field( 'sp_appid', 'Snowplow Application Id', 'wp_snowplow_setting_appid', 'general', 'snowplow');
 	register_setting('general', 'sp_appid');
 }
-add_action( 'admin_init', 'snowplow_settings_init' );
+add_action( 'admin_init', 'wp_snowplow_settings_init' );
 
 function snowplow_settings_section() {
 	echo '<p>This controls snowplow tracking configuration</p>';
 }
 
-function snowplow_setting_collector() {
+function wp_snowplow_setting_collector() {
 	$collector = get_option('sp_collector');
 	echo '<input name ="sp_collector" id="sp_collector" type="textfield" value="'. htmlspecialchars($collector). '">';
 }
 
-function snowplow_setting_appid() {
+function wp_snowplow_setting_appid() {
 	$application_id = get_option('sp_appid');
 	echo '<input name ="sp_appid" id="sp_collector" type="textfield" value="'. htmlspecialchars($application_id). '">';
+}
+
+
+function wp_snowplow_get_cookie_domain() {
+	$cookie_domain = COOKIE_DOMAIN;
+	if (!$cookie_domain) {
+		$site_url = get_site_url();
+		$host = parse_url($site_url, PHP_URL_HOST);
+		$cookie_domain = preg_replace('/^www\./', '', $host);
+	}
+	return $cookie_domain;
 }
