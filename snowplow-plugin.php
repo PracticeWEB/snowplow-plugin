ec3956c2808eb9ccb81d5edd78566d844513cdf4
David Robinson <David.Robinson@practiceweb.co.uk>
2017-09-11 17:04:59 +0100

    Cleanup and added tracker url as variable

--- snowplow-plugin.php
+++ snowplow-plugin.php
@@ -2,69 +2,108 @@
 
 /*
 Plugin Name: Snowplow Plugin
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
+Plugin URI: http://www.practiceweb.co.uk
+Description: Snowplow tracking.
 Version: 1.0
-Author: david robinson
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Author: David Robinson
+Author URI: http://www.practiceweb.co.uk
+License: Proprietary
 */
 
-add_action('wp_enqueue_scripts', 'wp_snowplow_add_scripts');
-function wp_snowplow_add_scripts() {
-    $collector = get_option('sp_collector');
-    $application_id = get_option('sp_appid');
-    if ($collector && $application_id) {
-		wp_enqueue_script( 'snowplow_loader', plugin_dir_url( __FILE__ ) . 'js/snowplowLoader.js' );
-		// Use localization to parameterise the script.
-	    $cookie_domain = wp_snowplow_get_cookie_domain();
-		$parameters = array(
-			'collector' => $collector,
-			'application_id' => $application_id,
-			'cookie_domain' => $cookie_domain,
-		);
-		wp_enqueue_script( 'snowplow_tracker', plugin_dir_url( __FILE__ ) . 'js/snowplowTracker.js' );
-	    wp_localize_script('snowplow_tracker', 'sp_data', $parameters);
-	}
-}
+function snowplow_add_scripts()
+{
+    $collector_url = get_option('snowplow-collector-url');
+    $application_id = get_option('snowplow-application-id');
+    $snowplow_tracker_url = get_option('snowplow-tracker-url', '//d1fc8wv8zag5ca.cloudfront.net/2.8.2/sp.js');
+    if ($collector_url && $application_id && $snowplow_tracker_url) {
+        // Loader script ensures that the snowplow library is loaded.
+        wp_enqueue_script('snowplow-loader', plugin_dir_url(__FILE__) . 'js/snowplowLoader.js');
+        $loader_data = array(
+            'tracker_url' => $snowplow_tracker_url,
+        );
+        wp_localize_script('snowplow-loader', 'loader_data', $loader_data);
 
-// TODO add settings
+        // Tracker script integrates the actual track events.
+        $cookie_domain = snowplow_get_cookie_domain();
+        $parameters = array(
+            'collector' => $collector_url,
+            'application_id' => $application_id,
+            'cookie_domain' => $cookie_domain,
+        );
+        wp_enqueue_script('snowplow_tracker', plugin_dir_url(__FILE__) . 'js/snowplowTracker.js');
+        wp_localize_script('snowplow_tracker', 'tracker_data', $parameters);
+    }
+}
+add_action('wp_enqueue_scripts', 'snowplow_add_scripts');
+
+
 /**
- * Collector
- * App ID
- *
+ * Add Snow plow settings.
  */
-function wp_snowplow_settings_init() {
-	add_settings_section( 'snowplow', 'Snowplow Settings', 'wp_snowplow_settings_section', 'general' );
-
-	add_settings_field( 'sp_collector', 'Snowplow Collector', 'wp_snowplow_setting_collector', 'general', 'snowplow');
-	register_setting('general', 'sp_collector');
-	add_settings_field( 'sp_appid', 'Snowplow Application Id', 'wp_snowplow_setting_appid', 'general', 'snowplow');
-	register_setting('general', 'sp_appid');
+function snowplow_settings_init()
+{
+    // Add a snowplow section to general.
+    add_settings_section('snowplow', 'Snowplow Settings', 'snowplow_settings_section', 'general');
+    // Tracker url
+    add_settings_field('snowplow-tracker-url', 'Snowplow Tracker url', 'snowplow_setting_tracker', 'general', 'snowplow');
+    register_setting('general', 'snowplow-tracker-url');
+    // Collector url.
+    add_settings_field('snowplow-collector-url', 'Snowplow Collector', 'snowplow_setting_collector', 'general', 'snowplow');
+    register_setting('general', 'snowplow-collector-url');
+    // Application Id
+    add_settings_field('snowplow-application-id', 'Snowplow Application Id', 'snowplow_setting_appid', 'general', 'snowplow');
+    register_setting('general', 'snowplow-application-id');
 }
-add_action( 'admin_init', 'wp_snowplow_settings_init' );
+add_action('admin_init', 'snowplow_settings_init');
 
-function snowplow_settings_section() {
-	echo '<p>This controls snowplow tracking configuration</p>';
+/**
+ * Render the settings section.
+ */
+function snowplow_settings_section()
+{
+    echo '<p>This controls snowplow tracking configuration</p>';
 }
 
-function wp_snowplow_setting_collector() {
-	$collector = get_option('sp_collector');
-	echo '<input name ="sp_collector" id="sp_collector" type="textfield" value="'. htmlspecialchars($collector). '">';
+/**
+ * Field for tracker url.
+ */
+function snowplow_setting_tracker()
+{
+    $tracker = get_option('snowplow-tracker-url', '//d1fc8wv8zag5ca.cloudfront.net/2.8.2/sp.js');
+    echo '<input name="snowplow-tracker-url" id="snowplow-tracker-url" type="textfield" value="' . htmlspecialchars($tracker) . '">';
 }
 
-function wp_snowplow_setting_appid() {
-	$application_id = get_option('sp_appid');
-	echo '<input name ="sp_appid" id="sp_collector" type="textfield" value="'. htmlspecialchars($application_id). '">';
+/**
+ * Field for collector url.
+ */
+function snowplow_setting_collector()
+{
+    $collector = get_option('snowplow-collector-url');
+    echo '<input name ="snowplow-collector-url" id="snowplow-collector-url" type="textfield" value="' . htmlspecialchars($collector) . '">';
 }
 
+/**
+ * Field for application id.
+ */
+function snowplow_setting_appid()
+{
+    $application_id = get_option('snowplow-application-id');
+    echo '<input name ="snowplow-application-id" id="snowplow-application-id" type="textfield" value="' . htmlspecialchars($application_id) . '">';
+}
 
-function wp_snowplow_get_cookie_domain() {
-	$cookie_domain = COOKIE_DOMAIN;
-	if (!$cookie_domain) {
-		$site_url = get_site_url();
-		$host = parse_url($site_url, PHP_URL_HOST);
-		$cookie_domain = preg_replace('/^www\./', '', $host);
-	}
-	return $cookie_domain;
+/**
+ * Helper to get the cookie domain for snowplow.
+ *
+ * @return string
+ *   The cookie domain.
+ */
+function snowplow_get_cookie_domain()
+{
+    $cookie_domain = COOKIE_DOMAIN;
+    if (!$cookie_domain) {
+        $site_url = get_site_url();
+        $host = parse_url($site_url, PHP_URL_HOST);
+        $cookie_domain = preg_replace('/^www\./', '', $host);
+    }
+    return $cookie_domain;
 }
